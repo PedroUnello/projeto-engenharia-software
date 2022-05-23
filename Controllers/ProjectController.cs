a794ddc2cd64b15587b58e52a08e06b7f6f383f5
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2022-05-22 21:23:30 -0300

    Criando endpoints de Project

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -13,11 +13,28 @@ namespace Burndown.Controllers
     [Route("api/Project")]
     public class ProjectController : ControllerBase
     {
+        [HttpGet("getAll")]
+        public async Task<ActionResult<Project>> GetAll([FromServices] IUnitOfWork uow, [FromServices] IProjectService service)
+        {
+            return Ok(await uow.Execute(() => service.GetAll()));
+        }
 
         [HttpPost("create")]
         public async Task<ActionResult<Project>> Create([FromServices] IUnitOfWork uow, [FromServices] IProjectService service, [FromBody] Project request)
         {
             return Ok(await uow.Execute(() => service.Create(request)));
+        }
+
+        [HttpPut("update")]
+        public async Task<ActionResult<Project>> Update([FromServices] IUnitOfWork uow, [FromServices] IProjectService service, [FromBody] Project request)
+        {
+            return Ok(await uow.Execute(() => service.Update(request)));
+        }
+
+        [HttpDelete("delete/{hash}")]
+        public async Task<ActionResult<Project>> Delete([FromServices] IUnitOfWork uow, [FromServices] IProjectService service, [FromRoute] Guid hash)
+        {
+            return Ok(await uow.Execute(() => service.Delete(hash)));
         }
     }
 }
--- Services/ProjectNS/IProjectService.cs
+++ Services/ProjectNS/IProjectService.cs
@@ -1,10 +1,16 @@
 ﻿using Burndown.Entities.ProjectNS;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Burndown.Services.ProjectNS
 {
     public interface IProjectService : IBaseService
     {
+        Task<IEnumerable<Project>> GetAll();
+        Task<Project> Get(Guid hash);
         Task<Project> Create(Project request);
+        Task<Project> Update(Project request);
+        Task<Project> Delete(Guid hash);
     }
 }
